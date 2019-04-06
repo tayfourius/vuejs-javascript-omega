3a402047db094c6fa8d1cfae942f5713b81926a1
tayfourius <abood.tayfour@gmail.com>
2019-04-06 18:04:15 +0200

    Update .babelrc, README.md, and 7 more files...

---
+++ Handling User Input with Forms Practice/src/FullName.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="fullName">
+        <p>Full Name: {‌{firstName}} {‌{lastName}} </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    },
+    props : ["firstName", "lastName"]
+    
+}
+</script>
+<style scoped>
+
+</style>
